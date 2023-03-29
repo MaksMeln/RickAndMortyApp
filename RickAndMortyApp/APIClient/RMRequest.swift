bb3cce10a18dd9adf1e6de0ac02c949d0ea16f0d
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-30 01:45:27 +0300

    Added docstrings and API related files

---
+++ RickAndMortyApp/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMortyApp
+//
+//  Created by Максим Мельничук on 30.03.23.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+
+}
