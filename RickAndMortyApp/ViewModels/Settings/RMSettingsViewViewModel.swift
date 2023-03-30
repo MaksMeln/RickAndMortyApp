508ee86030a0928b5320de564317f4957d74c4e9
MaksMeln <maksim.melnichuk_13@mail.ru>
2023-03-30 03:05:52 +0300

    Several Updates

---
+++ RickAndMortyApp/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMortyApp
+//
+//  Created by Максим Мельничук on 30.03.23.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
